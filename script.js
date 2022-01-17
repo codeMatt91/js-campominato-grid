d6a8dec18f35939b5357113448cd7ba208163ffe
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-17 16:33:02 +0100

    Added general function

--- script.js
+++ script.js
@@ -11,17 +11,42 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
 1- Recupero la scelta dell'utente
 2- chiaro le variabili che mi servono
-2- Creo la funzione che genera numero random ( per tutti e 3 i casi)
+2- Creo la funzione che genera numero random 
 3- Creo le 3 tipologie di griglia con una funzione
 4- creo le condizioni in base alla scelta
 5-riempio le condizioni per far si che si riempia alla scelta dell'utente 
 
 */
 
-//* Scelta dell'utente 
+//! ***********************************  FUNCTION    ************************************
+
+
+function getRandomNum(min, max){
+   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function generateCells(number){
+   for(let i = 0; i < number; i++){
+      const element = document.createElement('div');
+      element.className = 'cell';
+
+      element.append(getRandomNum(1,100));
+   }
+}
+
+
+
+
+//! ****************************************  LOGIC    ****************************************
+
+//* Recupero elementi in pagina 
 const userChoice = document.getElementById('select');
+const grid = document.getElementById('grid');
+const generate = document.getElementById('generate');
 
 //* Le mie variabili
 const level1 = 100;
 const level2 = 81;
 const level3 = 49;
+const list = [];
+
